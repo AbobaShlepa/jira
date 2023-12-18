5ff2c5bd010d47f7ddcd458e0873f3d308653552
Vladimir <your@email.example>
2023-12-18 12:38:58 +0200

    initial commit

---
+++ src/components/Board.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Column from './Column.vue'
+</script>
+
+
+<template>
+  <div class="board">
+    <Column title="ToDo" />
+    <Column title="Blocked" />
+    <Column title="Development" />
+    <Column title="Done" />
+  </div>
+</template>
+
+<style scoped>
+  .board {
+    display: flex;
+  }
+</style>
---
+++ src/components/Column.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div>
+    <div class="title">
+      <h3>{{ title }}</h3>
+    </div>
+    <div class="card">
+      Card 1
+    </div>
+    <div class="card">
+      Card 2
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  border: 2px solid black;
+  margin: 10px;
+  width: 150px;
+  height: 50px;
+  text-align: center;
+  vertical-align: middle;
+}
+
+.card {
+  border: 2px solid black;
+  margin: 10px;
+  width: 150px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
