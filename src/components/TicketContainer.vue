58e56ca288897eafd205b4c65bfe54fe04bad926
Vladimir <your@email.example>
2023-12-21 14:15:16 +0200

    Added animation for cards while moving

--- src/components/TicketContainer.vue
+++ src/components/TicketContainer.vue
@@ -17,10 +17,21 @@
     changeColumn(ticketId, targetColumnId);
   }
 
+  function addAnimation(event: any) {
+    const item: HTMLElement = event.item;
+    item.classList.add('shaking');
+  }
+
+  function removeAnimation(event: any) {
+    const item: HTMLElement = event.item;
+    item.classList.remove('shaking');
+  }
+
 </script>
 
 <template>
-  <Draggable :list="tickets" item-key="id" group="tickets" @end="log" tag="div" :component-data="{ id: columnId }">
+  <Draggable :list="tickets" item-key="id" group="tickets" @end="log" tag="div" :component-data="{ id: columnId }"
+    @choose="addAnimation" @unchoose="removeAnimation">
     <template #item="{ element }">
       <div class="card" :id="element.id">
         {{ element.title }}
@@ -28,3 +39,35 @@
     </template>
   </Draggable>
 </template>
+
+<style>
+.card:hover {
+  border-color: blue;
+}
+
+.shaking {
+  animation: tilt-shaking 0.15s infinite;
+}
+
+@keyframes tilt-shaking {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  25% {
+    transform: rotate(5deg);
+  }
+
+  50% {
+    transform: rotate(0eg);
+  }
+
+  75% {
+    transform: rotate(-5deg);
+  }
+
+  100% {
+    transform: rotate(0deg);
+  }
+}
+</style>
