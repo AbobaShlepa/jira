54de27a348e2cd88ae6ad899296604846733873d
Vladimir <your@email.example>
2023-12-28 10:35:45 +0200

    Added bottom menu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
   import Board from './components/Board.vue';
+  import SideMenu from './components/SideMenu.vue'
 </script>
 
 <template>
-  <Board />
+  <div class="flex">
+    <Board />
+    <SideMenu class="stick-right" />
+  </div>
 </template>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+
+.stick-right {
+  position: absolute;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+</style>
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  let shown = ref(true);
+
+  function toggleMenu() {
+    shown.value = !shown.value;
+  }
+
+</script>
+
+<template>
+  <div class="flex vertical">
+    <button @click="toggleMenu">
+      {{ shown ? 'Show' : 'Hide' }}
+    </button>
+    <div v-bind:style="{ visibility: shown ? 'visible' : 'hidden' }">
+      <button>
+        +
+      </button>
+
+      <button>
+        -
+      </button>
+
+      <button>
+        *
+      </button>
+
+      <button>
+        *
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.vertical {
+  writing-mode: vertical-rl;
+}
+
+button {
+  height: 100px;
+}
+</style>
