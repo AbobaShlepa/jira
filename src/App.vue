496b37236addceaafedcd12b9c771f7515d1c431
Vladimir <your@email.example>
2024-03-05 09:42:26 +0200

    Fixed board scroll

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-  import SideMenu from '@/components/board/SideMenu.vue';
-  import { onMounted } from 'vue';
-  import { getItem } from './stores/storageExtensions';
+import SideMenu from '@/components/board/SideMenu.vue';
+import { onMounted } from 'vue';
+import { getItem } from './stores/storageExtensions';
 
-  onMounted(function () {
-    const theme = getItem<string>('currentTheme') ?? 'dark';
-    document.body.setAttribute('data-theme', theme);
-  })
+onMounted(function () {
+  const theme = getItem<string>('currentTheme') ?? 'dark';
+  document.body.setAttribute('data-theme', theme);
+})
 </script>
 
 <template>
@@ -19,11 +19,17 @@
 </template>
 
 <style scoped>
-  .side-menu {
-    position: fixed;
-    display: flex;
-    gap: 10px;
-    right: 0;
-    top: 0;
-  }
+.side-menu {
+  position: fixed;
+  display: flex;
+  gap: 10px;
+  right: 0;
+  top: 0;
+}
+
+#content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
--- src/components/board/Board.vue
+++ src/components/board/Board.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
-  import Column from '@/components/column/Column.vue';
-  import { useColumnsStore } from '@/stores/columns';
-  import { usePermissionStore } from '@/stores/permissions';
-  import { useSprintStore } from '@/stores/sprints';
-  import { useTicketsStore } from '@/stores/tickets';
-  import { storeToRefs } from 'pinia';
-  import { computed } from 'vue';
-  import draggable from 'vuedraggable';
+import Column from '@/components/column/Column.vue';
+import { useColumnsStore } from '@/stores/columns';
+import { usePermissionStore } from '@/stores/permissions';
+import { useSprintStore } from '@/stores/sprints';
+import { useTicketsStore } from '@/stores/tickets';
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
+import draggable from 'vuedraggable';
 
-  const { columns, swapColumns } = useColumnsStore();
-  const { editColumns } = storeToRefs(usePermissionStore());
-  const { getTickets } = useTicketsStore();
-  const { currentSprintId } = storeToRefs(useSprintStore());
+const { columns, swapColumns } = useColumnsStore();
+const { editColumns } = storeToRefs(usePermissionStore());
+const { getTickets } = useTicketsStore();
+const { currentSprintId } = storeToRefs(useSprintStore());
 
-  function swap(event: any) {
-    const oldIndex = event.moved.oldIndex;
-    const newIndex = event.moved.newIndex
-    swapColumns(oldIndex, newIndex);
-  }
+function swap(event: any) {
+  const oldIndex = event.moved.oldIndex;
+  const newIndex = event.moved.newIndex
+  swapColumns(oldIndex, newIndex);
+}
 </script>
 
 <template>
@@ -30,8 +30,11 @@
 </template>
 
 <style scoped>
-  .board {
-    display: flex;
-    justify-content: safe center;
-  }
+.board {
+  display: flex;
+  max-width: 100vw;
+  overflow-x: auto;
+  overflow-y: hidden;
+  gap: 10px;
+}
 </style>
