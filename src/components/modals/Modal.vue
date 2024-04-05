d8d7942f41d4408e78c4bdb207622e72e3116e89
Vladimir <your@email.example>
2024-04-05 13:00:11 +0300

    Added modal animation

--- src/components/modals/Modal.vue
+++ src/components/modals/Modal.vue
@@ -1,69 +1,81 @@
 <script setup lang="ts">
-  import { usePermissionStore, type Permission } from '@/stores/permissions';
-  import { OnClickOutside } from '@vueuse/components';
+import { usePermissionStore, type Permission } from '@/stores/permissions';
+import { OnClickOutside } from '@vueuse/components';
 
-  const { togglePermission } = usePermissionStore();
+const { togglePermission } = usePermissionStore();
 
-  defineProps<{
-    permission: Permission
-  }>()
+defineProps<{
+  permission: Permission
+}>()
 </script>
 
 <template>
   <OnClickOutside @trigger="permission.enabled = false">
-    <div class="container modal" v-if="permission.enabled">
-      <div class="header">
-        <div class="header-content">
-          <slot name="header"></slot>
-          <button class="close" @click="togglePermission(permission)">X</button>
+    <Transition name="modal">
+      <div class="container modal" v-if="permission.enabled">
+        <div class="header">
+          <div class="header-content">
+            <slot name="header"></slot>
+            <button class="close" @click="togglePermission(permission)">X</button>
+          </div>
+        </div>
+        <div class="main">
+          <slot name="main"></slot>
         </div>
       </div>
-      <div class="main">
-        <slot name="main"></slot>
-      </div>
-    </div>
+    </Transition>
   </OnClickOutside>
 </template>
 
 <style scoped>
-  .container {
-    --width: 600px;
-    --height: 550px;
+.container {
+  --width: 600px;
+  --height: 550px;
 
-    background-color: var(--modal-background);
-    width: 600px;
-    height: 500px;
+  background-color: var(--modal-background);
+  width: 600px;
+  height: 500px;
 
-    border: 1px solid var(--modal-border);
-  }
+  border: 1px solid var(--modal-border);
+}
 
-  .modal {
-    position: absolute;
-    top: calc(50% - calc(var(--height) * 0.5));
-    left: calc(50% - calc(var(--width) * 0.5));
-  }
+.modal {
+  position: absolute;
+  top: calc(50% - calc(var(--height) * 0.5));
+  left: calc(50% - calc(var(--width) * 0.5));
+}
 
-  .header {
-    height: 40px;
-    border-bottom: 1px solid var(--subcontainer-background);
-    color: var(--modal-header-text);
-    background-color: var(--modal-header);
-  }
+.header {
+  height: 40px;
+  border-bottom: 1px solid var(--subcontainer-background);
+  color: var(--modal-header-text);
+  background-color: var(--modal-header);
+}
 
-  .close {
-    position: absolute;
-    top: 10px;
-    right: 20px;
-  }
+.close {
+  position: absolute;
+  top: 10px;
+  right: 20px;
+}
 
-  .main {
-    background-color: var(--modal-background);
-    color: var(--text-color);
-  }
+.main {
+  background-color: var(--modal-background);
+  color: var(--text-color);
+}
 
-  .main,
-  .header-content {
-    margin: 0 10px 0px 10px;
-    padding: 5px 0 0 0;
-  }
+.main,
+.header-content {
+  margin: 0 10px 0px 10px;
+  padding: 5px 0 0 0;
+}
+
+.modal-enter-active,
+.modal-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+}
 </style>
